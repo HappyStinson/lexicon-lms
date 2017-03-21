01042412980ea61f6d466d788e0ba2a30fc1fd49
Rasmus Nordling <rasmus.nordling@gmail.com>
2017-03-21 10:50:49 +0100

    add controller and views for managing Course

--- Lexicon-LMS/App_Start/RouteConfig.cs
+++ Lexicon-LMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Lexicon_LMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Courses", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Lexicon-LMS/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lexicon_LMS.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Kursnamn")]
+        [Required(ErrorMessage = "Du måste ange kursens namn")]
+        [StringLength(20, ErrorMessage = "Namnet kan inte vara längre än 20 tecken")]
+        public string Name { get; set; }
+
+        [Display(Name = "Kursbeskrivning")]
+        [StringLength(500, ErrorMessage = "Beskrivningen kan inte vara mer än 500 tecken")]
+        public string Description { get; set; }
+
+        [Display(Name = "Startdatum")]
+        [Required(ErrorMessage = "Du måste ange kursens startdatum")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Slutdatum")]
+        [Required(ErrorMessage = "Du måste ange kursens slutdatum")]
+        public DateTime EndDate { get; set; }
+
+        // Navigation property
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+    }
+}
