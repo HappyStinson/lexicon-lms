e8f929c2cf95ad830c41c346a3ead1599dcb842a
Jonas Lindström <Jonas.Lindstrom@bredband.net>
2017-03-22 14:35:12 +0100

    Ändra format för inmatning av datum i modellen för course

--- Lexicon-LMS/Models/Course.cs
+++ Lexicon-LMS/Models/Course.cs
@@ -19,10 +19,14 @@ namespace Lexicon_LMS.Models
 
         [Display(Name = "Startdatum")]
         [Required(ErrorMessage = "Du måste ange kursens startdatum")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:YYYY-MM-DD}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
 
         [Display(Name = "Slutdatum")]
         [Required(ErrorMessage = "Du måste ange kursens slutdatum")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:YYYY-MM-DD}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
 
         // Navigation properties
