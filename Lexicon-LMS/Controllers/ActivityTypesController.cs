d978ef5288271128116f26a7f28045fd201321be
Jonas Lindström <Jonas.Lindstrom@bredband.net>
2017-03-27 15:33:59 +0200

    Add ActivityTypesController and scaffold initial views for ActivityType

---
+++ Lexicon-LMS/Controllers/ActivityTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lexicon_LMS.Models;
+
+namespace Lexicon_LMS.Controllers
+{
+    public class ActivityTypesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ActivityTypes
+        public ActionResult Index()
+        {
+            return View(db.ActivityTypes.ToList());
+        }
+
+        // GET: ActivityTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            if (activityType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ActivityTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] ActivityType activityType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ActivityTypes.Add(activityType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            if (activityType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityType);
+        }
+
+        // POST: ActivityTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] ActivityType activityType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activityType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            if (activityType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityType);
+        }
+
+        // POST: ActivityTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            db.ActivityTypes.Remove(activityType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
